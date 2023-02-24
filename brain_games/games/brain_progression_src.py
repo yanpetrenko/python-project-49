c61edc9e61a4470aaee0fcb2f4a46fead66adef9
yanpetrenko <yanpetrenko@gmail.com>
2023-02-25 00:08:14 +0300

    final_8

--- brain_games/games/brain_progression_src.py
+++ brain_games/games/brain_progression_src.py
@@ -2,18 +2,19 @@ import random
 
 
 def progression():
-    question = []
+    list = []
     i = 0
     index = 0
     exponent = random.randint(1, 6)
     while i < 10:
         index += exponent
-        question.append(index)
+        list.append(index)
         i += 1
     index_random = random.randint(0, 9)
-    answer_true = question[index_random]
-    question[index_random] = '..'
-    return [question, str(answer_true)]
+    answer_true = list[index_random]
+    list[index_random] = ".."
+    question = str(list).replace("'",'').replace(",",'')
+    return [str(question)[1:-1:], str(answer_true)]
 
 
 def main():
